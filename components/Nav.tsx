ada26b097a6cbcdfc53439c1b1ca16151d164d84
Patryk Kurpiel <zeta.lacerta@gmail.com>
2022-09-01 03:15:09 +0200

    First commit.

---
+++ components/Nav.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import navStyles from "../styles/Nav.module.sass";
+
+const Nav = () => {
+  return (
+    <div className={navStyles["main-nav"]}>
+      <div className={navStyles["main-nav__nav-container"]}>
+        <ul className={navStyles["main-nav__nav-container__ul"]}>
+          <li>News</li>
+          <li>Matches</li>
+          <li>Results</li>
+          <li>Events</li>
+          <li>Stats</li>
+          <li>Galleries</li>
+          <li>Forums</li>
+          <li>Betting</li>
+          <li>Live</li>
+          <li>Fantasy</li>
+        </ul>
+        <form
+          className={navStyles["main-nav__nav-container__search-container"]}
+        >
+          <input type="text" placeholder="Search..." />
+          <FontAwesomeIcon icon={faSearch} />
+        </form>
+        <div className={navStyles["main-nav__nav-container__sign-in"]}>
+          Sign in
+        </div>
+        <div className={navStyles["main-nav__nav-container__nav-down"]}>
+          <FontAwesomeIcon icon={faCaretDown} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
